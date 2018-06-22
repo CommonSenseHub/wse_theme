79d19ffa83fb581216b0356c364838c11bf679a4
Mauricio Dulce <hola@mauriciodulce.com>
2018-06-22 10:02:56 -0500

    agregando hero

--- templates/front-page.php
+++ templates/front-page.php
@@ -12,6 +12,9 @@ if ( ! class_exists( 'Timber' ) ) {
   echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
   return;
 }
+if ( isset( $post->hero_image_desktop ) && strlen( $post->hero_image ) ) {
+    $post->hero_image = new Timber\Image( $post->hero_image );
+}
 
 $context = Timber::get_context();
 $post = new TimberPost();
